e158904050f0593d3649045a3071f9f49ecef665
Meikuyh <54567017+Meikuyh@users.noreply.github.com>
2019-08-27 10:54:53 +0700

    Update packet.js

--- packet.js
+++ packet.js
@@ -1,2 +1,27 @@
-asdadasd
-dsaasdasdasdadsdas
+{
+	"name": "Worldmart Toolkit",
+	"version": "1.1.6",
+	"download_url": "http://plugins.kutethemes.net/worldmart-toolkit.zip",
+
+	"homepage": "https://worldmart.kutethemes.net/",
+	"requires": "4.5",
+	"tested": "4.8",
+	"last_updated": "2019-27-08 16:17:00",
+	"upgrade_notice": "Update new functions",
+
+	"author": "kutetheme",
+	"author_homepage": "https://kutethemes.com/",
+
+	"sections": {
+		"description": "(Required) Plugin description. Basic HTML can be used in all sections.",
+		"installation": "(Recommended) Installation instructions.",
+		"changelog": "(Recommended) Changelog. <p>This section will be displayed by default when the user clicks 'View version x.y.z details'.</p>",
+		"custom_section": "This is a custom section labeled 'Custom Section'."
+	},
+
+	"rating": 90,
+	"num_ratings": 123,
+
+	"downloaded": 1234,
+	"active_installs": 12345
+}
